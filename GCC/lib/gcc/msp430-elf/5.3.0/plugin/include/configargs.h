e786eccee8d18eb4bc56b13f9c088085d5e4bf34
Chris Bonkowski <Chris@uofs-10-227-1-204.usask.ca>
2017-01-28 12:58:16 -0600

    Adding GCC Compiler

---
+++ GCC/lib/gcc/msp430-elf/5.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/srv/net/msp430/jenkins/workspace/msp430-gcc-5-pkg-gnu/gcc.subtree/configure --build=x86_64-redhat-linux --host=x86_64-apple-darwin15 --target=msp430-elf --enable-languages=c,c++ --disable-nls --enable-target-optspace --enable-newlib-nano-formatted-io --with-pkgversion='SOMNIUM Technologies Limited - msp430-gcc 5.3.0.224'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
